4fe2f556d4b9602d42918a6d578b8a685d19ea1e
Harsh Verma <verma.harsh98@gmail.com>
2019-10-04 11:45:12 +0530

    Updates

---
+++ precache-manifest.5f721cb2d9903bc734ae8f0f6e82a314.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a328b65145307dc7d4fad1e0404978",
+    "url": "/Whitepanda/index.html"
+  },
+  {
+    "revision": "0bc9327d0b3d02b0e5db",
+    "url": "/Whitepanda/static/css/2.56877a25.chunk.css"
+  },
+  {
+    "revision": "56985781df4ed8237754",
+    "url": "/Whitepanda/static/css/main.f534e426.chunk.css"
+  },
+  {
+    "revision": "0bc9327d0b3d02b0e5db",
+    "url": "/Whitepanda/static/js/2.6af0f6d6.chunk.js"
+  },
+  {
+    "revision": "56985781df4ed8237754",
+    "url": "/Whitepanda/static/js/main.65d5e3c0.chunk.js"
+  },
+  {
+    "revision": "09875d97d5858cd6a5ec",
+    "url": "/Whitepanda/static/js/runtime-main.f9234f66.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Whitepanda/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Whitepanda/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Whitepanda/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Whitepanda/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Whitepanda/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
